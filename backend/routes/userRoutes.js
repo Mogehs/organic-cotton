090275b188c4dedf05f28fd1ed1406dd6cdec186
Mogehs <mugheesurrehman06@gmail.com>
2025-04-24 15:22:13 +0500

    added backend route updaed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,26 +16,16 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Auth
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
-// OTP Verification
 router.post("/verify-otp/:id", verifyOTP);
 router.post("/resend-otp", resendOTP);
-
-// Password reset
 router.post("/request-password-reset", requestPasswordReset);
 router.post("/reset-password", resetPassword);
-
-// User Profile
 router.get("/profile/:id", authMiddleware, getUserProfile);
 router.put("/profile/:id", authMiddleware, updateUserProfile);
-
-// Admin-only
 router.get("/", authMiddleware, getAllUsers);
 router.delete("/:id", authMiddleware, deleteUser);
-
 router.post("/logout", logoutUser);
 
 export default router;
